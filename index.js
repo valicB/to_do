f45737de88fff6ca1a6c26b20ffd386be0c36ba0
valicB <valentinbriceag@gmail.com>
2017-07-07 19:06:52 +0300

    Update index.js

--- index.js
+++ index.js
@@ -9,30 +9,46 @@ var server = http.createServer((req,res)=>{
     // action/entity/id
     var url = req.url;
     [x,action,entity,id] = url.split("/");
-    // console.log(id);
+    var tasks = fs.readdirSync('./database/');
+    // console.log(tasks);
     if(action == "create" && entity == "task"){
         if(id !== undefined){
+          if(!fs.existsSync('./database/' + id + '.json')){
             fs.writeFileSync('./database/' + id + '.json', JSON.stringify({ id: id }));
-
-        }
+            res.write('File ' + id + '.json was created');
+          } else res.write('File ' + id + '.json exists' + fs.readFileSync('./database/' + id + '.json'));
+        } else res.write('The filename is not set. Please set the filename');
     } else if(action == "show" && entity == "task"){
         if(id !== undefined){
-            if (fs.existsSync('./database/' + id + '.json')) {
-                var task = fs.readFileSync('./database/' + id + '.json');
-                res.write(task);
-            }
+            if(fs.existsSync('./database/' + id + '.json')) res.write(fs.readFileSync('./database/' + id + '.json'));
+            else res.write('404. File not found');
+        } else{
+            if(fs.existsSync('./database/' + id + '.json')){
+              tasks.forEach(function(item) {
+                fs.readFileSync('./database/' + item);
+                res.write(fs.readFileSync('./database/' + item));
+              });
+            } else res.write('No more files found');
+        }
+    } else if (action == "delete" && entity == "task") {
+        if (id !== undefined){
+          if(fs.existsSync('./database/' + id + '.json')){
+            fs.unlinkSync('./database/' + id + '.json');
+            res.write('File ' + id + '.json was deleted');
+          }
+          else res.write('File ' + id + '.json dont exist');
+        } else {
+            if(tasks[0]){
+              tasks.forEach(function(item) { fs.unlinkSync('./database/' + item) });
+              res.write('Files was deleted');
+            } else res.write('No more files found');
         }
     } else {
-        var tasks = fs.readdirSync('./database');
-        for(var i=0; i<tasks.length; i++){
-            var task = fs.readFileSync('./database/' + tasks[i]);
-            res.write(task);
-        }
-        // console.log(tasks);
+        res.write('404. Page not found');
     }
     /////////////////////////////////////
 
-    res.end("All good!");
+    res.end();
 });
 server.listen(config.options.port);
 console.log("SERVER RUNNING PORT: " + config.options.port);
